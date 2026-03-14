dd4c8319fdd7cb5ffad6fb2c8edb9f38289e89b7
Chartzh <rajifraditia@gmail.com>
2026-03-14 11:55:21 +0700

    change adapter to use vercel

--- .svelte-kit/output/server/manifest-full.js
+++ .svelte-kit/output/server/manifest-full.js
@@ -7,10 +7,10 @@ function __memo(fn) {
 return {
 	appDir: "_app",
 	appPath: "_app",
-	assets: new Set([]),
-	mimeTypes: {},
+	assets: new Set(["favicon.svg"]),
+	mimeTypes: {".svg":"image/svg+xml"},
 	_: {
-		client: {start:"_app/immutable/entry/start.C1Qv0qQj.js",app:"_app/immutable/entry/app.DNNLm5qg.js",imports:["_app/immutable/entry/start.C1Qv0qQj.js","_app/immutable/chunks/Dfkvhwmb.js","_app/immutable/chunks/mQTrEUch.js","_app/immutable/chunks/bkYrbMlf.js","_app/immutable/entry/app.DNNLm5qg.js","_app/immutable/chunks/mQTrEUch.js","_app/immutable/chunks/R353CW9e.js","_app/immutable/chunks/D7wKw-Lh.js","_app/immutable/chunks/bkYrbMlf.js","_app/immutable/chunks/Dw0VDt_Q.js","_app/immutable/chunks/G30fl7Mi.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
+		client: {start:"_app/immutable/entry/start.P4LBWQh2.js",app:"_app/immutable/entry/app.Yio-lqFr.js",imports:["_app/immutable/entry/start.P4LBWQh2.js","_app/immutable/chunks/C1pHcHCY.js","_app/immutable/chunks/DomnpJ65.js","_app/immutable/entry/app.Yio-lqFr.js","_app/immutable/chunks/DomnpJ65.js","_app/immutable/chunks/DhCwh3CP.js","_app/immutable/chunks/MBwLGG55.js","_app/immutable/chunks/CPLwK8NW.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
 		nodes: [
 			__memo(() => import('./nodes/0.js')),
 			__memo(() => import('./nodes/1.js')),
--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel'; // Ubah baris ini
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
