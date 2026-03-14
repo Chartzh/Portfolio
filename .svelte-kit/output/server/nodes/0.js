94b48b629a5c5d71b8abf05608bc65ee97b0db70
Chartzh <rajifraditia@gmail.com>
2026-03-14 12:21:11 +0700

    fix: move deps, ignore .svelte-kit

--- .svelte-kit/output/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.vG59q_Oa.js","_app/immutable/chunks/DhCwh3CP.js","_app/immutable/chunks/DomnpJ65.js","_app/immutable/chunks/CPLwK8NW.js","_app/immutable/chunks/DqIcKYkg.js"];
-export const stylesheets = ["_app/immutable/assets/0.CrCH_oc4.css"];
-export const fonts = [];
--- .svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.jz7oqffz.js","_app/immutable/chunks/DhCwh3CP.js","_app/immutable/chunks/DomnpJ65.js","_app/immutable/chunks/DER-5FvN.js","_app/immutable/chunks/C1pHcHCY.js"];
-export const stylesheets = [];
-export const fonts = [];
