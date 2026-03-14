064e74f3c8dd4bb2eebf7de463c97c587d0810f0
Chartzh <rajifraditia@gmail.com>
2026-03-14 13:08:23 +0700

    fix: remove .vercel output from repo

--- .vercel/output/functions/![-]/catchall.func/.svelte-kit/output/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.5sFLjPeQ.js","_app/immutable/chunks/DhCwh3CP.js","_app/immutable/chunks/DomnpJ65.js","_app/immutable/chunks/DqIcKYkg.js","_app/immutable/chunks/DER-5FvN.js","_app/immutable/chunks/MBwLGG55.js","_app/immutable/chunks/CPLwK8NW.js"];
-export const stylesheets = ["_app/immutable/assets/2._3ZKjR6J.css"];
-export const fonts = [];
